f1f8be5b0fd94118f2a0a40b8205b9b6ff79265d
Isuru Chathuranga Bandara <icbandara@pcugroup.com>
2023-11-06 10:48:29 +0530

    router added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import * as path from "path";
+import {FileUploaderComponent} from "./file-uploader/file-uploader.component";
+import {FileComparisonComponent} from "./file-comparison/file-comparison.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '/file-upload', component: FileUploaderComponent},
+  {path: '/farmer-comparison', component: FileComparisonComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
